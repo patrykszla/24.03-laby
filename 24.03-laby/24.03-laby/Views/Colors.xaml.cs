705bea29c11a3eceb38b21be0b057bceba7d8726
Patryk Szlachta <patryk.szla@interia.pl>
2020-03-26 21:42:49 +0100

    task 1.4 lab2

---
+++ 24.03-laby/24.03-laby/Views/Colors.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace 24.03-laby.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class Colors : ContentPage
+{
+    public Colors()
+    {
+        InitializeComponent();
+    }
+}
+}
